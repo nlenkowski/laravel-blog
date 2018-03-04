6a1830cd27618124a1577f4d5afb025b3e40a6cb
Nathan Lenkowski <nathan@littlebiglab.com>
2018-03-04 16:49:51 +0100

    Update browsersync proxy uri

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,4 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
-   .browserSync('laravel-blog.lndo.site');
+   .browserSync('laravel-blog.test');
