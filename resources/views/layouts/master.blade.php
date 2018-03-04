8bb4368b746c2183bd76c5ef8a35bf51aa4e1670
Nathan Lenkowski <nathan@littlebiglab.com>
2018-03-04 17:55:58 +0100

    Add blog views

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,23 +1,29 @@
-
 <!doctype html>
 <html lang="en">
   <head>
-    <title>Album example for Bootstrap</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
+
+    <title>Blog Template for Bootstrap</title>
     <link rel="icon" href="favicon.ico">
     <link href="/css/app.css" rel="stylesheet">
   </head>
 
   <body>
 
-    @include('layouts.nav')
-
     <div class="container">
-      @yield('content')
+      @include('layouts.header')
+      @include('partials.jumbotron')
     </div>
+
+    <main role="main" class="container">
+      <div class="row">
+        @yield('content')
+        @include('layouts.sidebar')
+      </div>
+    </main>
 
     @include('layouts.footer')
 
