3ee7d6357bda346f0f687a5172185e3cc6069507
Nathan Lenkowski <nathan@littlebiglab.com>
2018-03-12 22:48:48 +0100

    Add comments to post show view

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,9 +2,20 @@
 
 @section('content')
 
-<div class="col-sm-8 blog-main">
-  <h1>{{ $post->title}}</h1>
-  <p>{{ $post->body }}</p>
-</div>
+  <div class="col-sm-8 blog-main">
+    <h1>{{ $post->title}}</h1>
+    <p>{{ $post->body }}</p>
+    <hr>
+    <h4>Comments</h4>
+    <div class="comments">
+      @foreach ($post->comments as $comment)
+      <li class="list-group-item">
+        <strong>{{ $comment->created_at->diffForHumans() }} </strong>
+        <br>
+        {{ $comment->body }}
+      </li>
+      @endforeach
+    </div>
+  </div>
 
 @endsection
