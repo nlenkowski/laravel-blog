b371fcb0d5eee5625517c24ac5e99efa7b7bfbd0
Nathan Lenkowski <nathan@littlebiglab.com>
2018-03-13 16:10:48 +0100

    Refactor add comment method to use eloquent relationship

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -7,13 +7,9 @@ use App\Comment;
 
 class CommentsController extends Controller
 {
-    public function store(Post $post) {
-
-        Comment::create([
-            'body' => request('body'),
-            'post_id' => $post->id
-        ]);
-
+    public function store(Post $post)
+    {
+        $post->addComment(request('body'));
         return back();
     }
 }
--- app/Post.php
+++ app/Post.php
@@ -8,4 +8,17 @@ class Post extends Model
   {
     return $this->hasMany(Comment::class);
   }
+
+  public function addComment($body)
+  {
+
+    // Use eloquent relationship to add comment
+    $this->comments()->create(compact('body'));
+
+    // Use long form method to add comment
+    // Comment::create([
+    //   'body' => $body,
+    //   'post_id' => $this->id
+    // ]);
+  }
 }
