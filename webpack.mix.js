e8872d0b4510d211eb25ee0e7f8bfd91fb66c65c
Nathan Lenkowski <nathan@littlebiglab.com>
2018-03-14 14:44:43 +0100

    Disable browsersync notification
    It was driving me nuts!

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,7 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
-   .browserSync('laravel-blog.test');
+   .browserSync({
+     proxy: 'laravel-blog.test',
+     notify: false
+    });
