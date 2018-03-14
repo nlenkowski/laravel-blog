ae8e3b94cc08dcb1f77e03a5a7bde3a6213d98b3
Nathan Lenkowski <nathan@littlebiglab.com>
2018-03-14 15:20:48 +0100

    Protect create and store session actions from authenticated users

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 class SessionsController extends Controller
 {
 
+    // Prohibit authenticated users from accessing create and store methods
+    public function __construct()
+    {
+        $this->middleware('guest')->except('destroy');
+    }
+
     // Create session
     public function create()
     {
