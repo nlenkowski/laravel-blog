223d85475896a5742b3ca16e236472e70d596509
Nathan Lenkowski <nathan@littlebiglab.com>
2018-03-14 10:48:36 +0100

    Add user_id to Post and Comment models

--- app/Comment.php
+++ app/Comment.php
@@ -5,7 +5,16 @@ namespace App;
 class Comment extends Model
 {
     // Get the post a comment belongs to
-    public function post() {
+    // $comment->post
+    public function post()
+    {
         return $this->belongsTo(Post::class);
+    }
+
+    // Get the user a comment belongs to
+    // $comment->user->name
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
--- app/Post.php
+++ app/Post.php
@@ -5,8 +5,16 @@ namespace App;
 class Post extends Model
 {
   // Get all comments for a post
+  // $post->comments
   public function comments()
   {
     return $this->hasMany(Comment::class);
+  }
+
+  // Get the user a post belongs to
+  // $post->user->name
+  public function user()
+  {
+    return $this->belongsTo(User::class);
   }
 }
